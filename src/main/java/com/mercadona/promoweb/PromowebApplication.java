4e98a123d96850ea3670e97573212c0b13336436
Getm26 <metiennedev@gmail.com>
2023-05-21 12:20:18 +0200

    initial commit project

---
+++ src/main/java/com/mercadona/promoweb/PromowebApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.promoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromowebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadona/promoweb/PromowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.promoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
