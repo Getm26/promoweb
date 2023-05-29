8ab2d5ee636fce83b4aef5b7e058feb7428f610e
Getm26 <metiennedev@gmail.com>
2023-05-30 01:55:50 +0200

    update class and interfaces

--- src/main/java/com/mercadona/promoweb/product/Product.java
+++ src/main/java/com/mercadona/promoweb/product/Product.java
@@ -2,81 +2,21 @@ package com.mercadona.promoweb.product;
 
 import javax.swing.ImageIcon;
 
-import org.springframework.stereotype.Indexed;
+import com.mercadona.promoweb.promotion.Promotion
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.net.URL;
 
-@Entity
+
 public class Product {
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String labelname;
+    private String label;
     private String description;
-    private ImageIcon picture;
+    private URL picture;
     private Integer price;
     private Integer discount;
-    private String category;
-        
-    
+    private Category category;
+    private Promotion promotion;
     public Product() {
     }
 
-    public Product(Long id, String labelname, String description, ImageIcon picture, Integer price, Integer discount,  String category) {
-        this.id = id;
-        this.labelname = labelname;
-        this.description = description;
-        this.picture = picture;
-        this.price = price;
-        this.discount = discount;
-        this.category = category;
-    }
 
-
-    public Product(String labelname, String description, ImageIcon picture, Integer price, Integer discount, String category) {
-        this.labelname = labelname;
-        this.description = description;
-        this.picture = picture;
-        this.price = price;
-        this.discount = discount;
-        this.category = category;
-    
-    }
-
-
-    public Product(String labelname) {
-        this.labelname = labelname;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getLabelname() {
-        return labelname;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public ImageIcon getPicture() {
-        return picture;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public Integer getDiscount() {
-        return discount;
-    }
-
-    public String getCategory() {
-        return category;
-
-
-}
